1a105884270834c473da03775e7cafdeb3ac5828
YEJUN7 <sikickpoce@naver.com>
2020-01-28 11:04:52 +0900

    create to ex1.js

---
+++ ex1.js
@@ -0,0 +1,20 @@
+var txt1 = 'text1';
+console.log(txt1);
+if (true) {
+    var txt2 = "text2";
+    console.log(txt2);
+}
+let txt3 = "text3";
+const txt4  ="yejun"; //변경불가능// 
+console.log(txt3);
+console.log(txt4);
+txt3 = "lee";
+console.log(txt3);
+txt3 = 500;
+txt4 = "park";
+console.log(txt4);
+if(true){
+    let txt5 = "you";
+    console.log(txt5);
+}
+console.log(txt5);
